168b1b40ce765568347f04b7d83e3d614d72f18a
Saif-ul Hussain <saif-ulhussain@saif-uls-air.home>
2022-07-26 12:27:09 +0100

    Created Transaction Class

---
+++ transaction.js
@@ -0,0 +1,11 @@
+class Transaction {
+
+  constructor (type, amount, balance) {
+    this.type = type;
+    this.amount = amount;
+    this.balance = balance;
+  }
+
+}
+
+module.exports = Transaction;
