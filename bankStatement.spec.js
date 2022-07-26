3f982655e92c244d74509324f31d85135becaa49
Saif-ul Hussain <saif-ulhussain@saif-uls-air.home>
2022-07-26 19:07:34 +0100

    Created test case to generate bank statement

--- bankStatement.spec.js
+++ bankStatement.spec.js
@@ -7,4 +7,17 @@ describe('BankStatement', () => {
     const bankStatement = new BankStatement(transactionHistory);
     expect(bankStatement.printHeader()).toEqual("date || credit || debit || balance");
   });
+  it('generates a statement containing debit and credit', () => {
+    const transactionHistory = new TransactionHistory();
+    const testDate = new Date()
+    const testData = {
+      date: testDate,
+      type: credit,
+      amount: 500,
+      balance: 1000
+    }
+    transactionHistory.transactions.push(testData);
+    const bankStatement = new BankStatement(transactionHistory);
+    expect(bankStatement.printStatement()).toEqual();
+  });
 });
